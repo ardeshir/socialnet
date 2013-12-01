6bf4d1b6ec304affd56178ab0ab467e09a9fa9e0
ardeshir sepahsalar <ardeshir.org@gmail.com>
2013-12-01 19:16:47 +0000

    Updated layout

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var app = express();
 
 app.configure(function() {
 	app.set('view engine', 'jade');
+	app.set('view options', {layout:true});
+	app.set('views', __dirname + '/views');
 	app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function(req, res) {
-	res.render('index.jade', {layout:false});
+	res.render('index.jade');
 })
 
 app.listen(8787); 
